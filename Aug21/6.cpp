1ac9750795a91e8d4865b0d1c5a0f681240b4e06
s10singh97 <s10singh97@yahoo.in>
2018-08-21 15:09:39 +0530

    extern variable

---
+++ Aug21/6.cpp
@@ -0,0 +1,18 @@
+// extern variable implementation
+
+#include<iostream>
+#include"7.cpp"
+using namespace std;
+int z = 10;
+
+int main(int argc, char const *argv[])
+{
+    int b = i + 80;
+    cout<<b;
+    i = 100;
+    cout<<"\ni = "<<i;
+    cout<<"\n#################################\n";
+    cout<<z<<"\n"<<y;
+    cout<<"\n"<<++a;
+    return 0;
+}
---
+++ Aug21/7.cpp
@@ -0,0 +1,5 @@
+// Supporting file for implementing extern in 6.cpp
+extern int i = 10;
+extern int z;
+int y = z + 10;
+int a = a + 800;
