df10e97cc9118cc29ac7714cd287e49ad3846b30
s10singh97 <s10singh97@yahoo.in>
2018-08-30 00:11:33 +0530

    renaming folder

--- 2017kucp1054_assignment2/2.cpp
+++ 2017kucp1054_assignment2/2.cpp
@@ -1,33 +1,53 @@
-//Without using Default Constructor
+//Finding maximum number
 
 #include<iostream>
 using namespace std;
 
-class Test
+class Test1
 {
+    int a;
     public:
-    static int count;
-    void modifyCount()
+    friend void max();
+    void assignValue1(int x)
     {
-        count++;
+        a = x;
     }
-    static void displayCount()
+    int displayvalue1()
     {
-        cout<<"\nObject Count : "<<count<<"\n";
+        return a;
     }
 };
 
-int Test::count = 0;
+class Test2
+{
+    int b;
+    public:
+    friend void max();
+    void assignValue2(int y)
+    {
+        b = y;
+    }
+    int displayvalue2()
+    {
+        return b;
+    }
+};
+
+int max(int x, int y)
+{
+    Test1 t1;
+    Test2 t2;
+    t1.assignValue1(x);   //Assigning value x to variable a of class Test1
+    t2.assignValue2(y);   //Assigning value y to variable b of class Test2
+    if(t1.displayvalue1() > t2.displayvalue2())
+        return t1.displayvalue1();
+    else
+        return t2.displayvalue2();
+}
 
 int main(int argc, char const *argv[])
 {
-    cout<<"No. of objects before creating objects: ";
-    Test::displayCount();
-    Test t1;
-    Test t2;
-    t1.modifyCount();
-    t2.modifyCount();
-    cout<<"\nAfter creating two objects : ";
-    Test::displayCount();
+    int max_number = max(10, 5);
+    cout<<"Maximum number is: "<<max_number;
     return 0;
 }
