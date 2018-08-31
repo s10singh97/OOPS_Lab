66594c0bd2f3d6fa384360aa1d6d7bfe0ccc3e17
s10singh97 <s10singh97@yahoo.in>
2018-08-31 15:08:17 +0530

    small mods in 2.cpp in assignment 3

--- 2017kucp1054_assignment3/2.cpp
+++ 2017kucp1054_assignment3/2.cpp
@@ -5,21 +5,26 @@ class Addition
 {
     int a, b, sum;
     public:
-    friend int add(int, int);
+    friend Addition add(int, int);
+    void show()
+    {
+        cout<<sum;
+    }
 };
 
-int add(int p, int q)
+Addition add(int p, int q)
 {
     Addition ob;
     ob.a = p;
     ob.b = q;
     ob.sum = ob.a + ob.b;
-    return ob.sum;
+    return ob;
 }
 
 int main(int argc, char const *argv[])
 {
-    int result = add(4, 5);
-    cout<<"Sum: "<<result;
+    Addition obj = add(4, 5);
+    cout<<"Sum: ";
+    obj.show();
     return 0;
 }
