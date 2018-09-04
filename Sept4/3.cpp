281e71496011f7aa46e0a195fdf26a99bd7bb18d
s10singh97 <s10singh97@yahoo.in>
2018-09-04 14:53:24 +0530

    Copy Constructor

--- Sept4/3.cpp
+++ Sept4/3.cpp
@@ -1,8 +1,27 @@
-//
-
+// Copy Constructor
+#include<iostream>
 using namespace std;
+
+class Test
+{
+    int a;
+    public:
+    Test()
+    {
+        a = 3;
+        cout<<"Default Constructor\n";
+    }
+    Test(Test &)
+    {
+        a = 5;
+        cout<<"Copy Constructor\n";
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    
+    Test t;
+    Test b(t);
+    Test c = b;
     return 0;
 }
