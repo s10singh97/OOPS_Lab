94db6228706e95d4fd6234b873b67a1a5b992da4
s10singh97 <s10singh97@yahoo.in>
2018-09-04 14:40:43 +0530

    Equality w/o operators

--- Sept4/1.cpp
+++ Sept4/1.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int a = 1, b = 1;
+    int a = 2, b = 1;
     int c = a ^ b;
+    if(c)
+    {
+        cout <<"Not Equal";
+    }
+    else
+        cout<<"Equal";
     return 0;
 }
