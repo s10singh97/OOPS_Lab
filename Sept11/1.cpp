228bf74c7595ac75a88b28fde6f170f270f1658e
s10singh97 <s10singh97@yahoo.in>
2018-09-11 14:20:58 +0530

    Testing Input methods and Output Methods

---
+++ Sept11/1.cpp
@@ -0,0 +1,22 @@
+// Testing Input methods and Output Methods
+
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    char* a;
+    cin.get(a, 10, ' ');
+    cout<<a<<"\n";
+    char* b;
+    cin.getline(b, 10, '\t');
+    cout<<b<<"\n";
+    char* c;
+    cin.ignore(' ');
+    cout<<c;
+    int i;
+    for (i = 0; a[i] != '\0'; i++);
+    int len = i;
+    for (i = 0; i < len; i++)
+        cout.put(a[i]);
+    return 0;
+}
