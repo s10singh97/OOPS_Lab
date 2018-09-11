75b42f1d71eb2ea5a7684e86c87530bc9fbc4044
s10singh97 <s10singh97@yahoo.in>
2018-09-11 14:53:43 +0530

    Mods in File I/O

--- Sept11/2.cpp
+++ Sept11/2.cpp
@@ -7,12 +7,19 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int b;
+    string s = "Hello World!!!", ss;
+
     ofstream a("a.txt");
-    a<<12;
+    if(a.is_open())
+        cout<<"File Opened Successfully to write\n";
+    a.write((char *)&s, sizeof(s));
     a.close();
+    
     ifstream c("a.txt");
-    c>>b;
-    cout<<b;
+    if(c.is_open())
+        cout<<"File Opened Successfully to read\n";
+    c.read((char*)&ss, sizeof(ss));
+    cout<<"Data in file is: "<<ss;
     c.close();
     return 0;
 }
