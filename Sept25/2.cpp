48ce9de7eb41fe82b1b349a3920b41043b2e740b
s10singh97 <s10singh97@yahoo.in>
2018-09-25 14:41:40 +0530

    Reading from one file nd writing to another in just ONE INSTRUCTION

---
+++ Sept25/2.cpp
@@ -0,0 +1,20 @@
+// Reading from one file nd writing to another in just ONE INSTRUCTION
+
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+void one_instruction_write(ifstream &ifs, ofstream &ofs)
+{
+    ofs<<ifs.rdbuf();
+}
+
+int main(int argc, char const *argv[])
+{
+    ifstream ifs;
+    ifs.open("fread.txt");
+    ofstream ofs;
+    ofs.open("fwrite.txt");
+    one_instruction_write(ifs, ofs);
+    return 0;
+}
