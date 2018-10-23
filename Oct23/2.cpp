e8b86cb1321fd33d9668ac275ff34967ebbfedb0
Shashwat <s10singh97@yahoo.in>
2018-10-23 14:10:36 +0530

    Type Converions(User-Defined to Basic)

---
+++ Oct23/2.cpp
@@ -0,0 +1,35 @@
+// Type Converions(User-Defined to Basic)
+
+#include<iostream>
+using namespace std;
+
+class A
+{
+    int a, b;
+    public:
+    void setValues(int x, int y)
+    {
+        a = x; b = y;
+    }
+    A()
+    {
+        a = 0;
+    }
+    operator int()          //Using conversion function
+    {
+        return (a+b);
+    }
+    void display()
+    {
+        cout<<a;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    A obj;
+    obj.setValues(4, 6);
+    int x = obj;
+    cout<<x;
+    return 0;
+}
